92dfc6611f6d3180af249d5cdc5b448e0fd6eec4
karina-rodriguez <karminer60@gmail.com>
2020-10-13 13:13:07 -0700

    met Day 1 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,23 +7,52 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.pc = 0
+        self.ir = None
+        self.ram = [0] * 256
+        self.sp = 256
+        self.commands = {
+            0b00000001: self.hlt,
+            0b10000010: self.ldi,
+            0b01000111: self.prn,
+            
+        }
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def hlt(self, operand_a, operand_b):
+        return (0, False)
+
+    def ldi(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        return (3, True)
+
+    def prn(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        return(2, True)
+    
+ 
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+       
 
         program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
+            
+            0b10000010, 
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111, 
             0b00000000,
-            0b00000001, # HLT
+            0b00000001, 
         ]
 
         for instruction in program:
@@ -36,7 +65,7 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+       
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +77,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +89,21 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        
+        while running:
+            instruction_register = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            try:
+                f = self.commands[instruction_register]
+               
+                operation_op = f(operand_a, operand_b
+                                 )
+                running = operation_op[1]
+                self.pc += operation_op[0]
+
+            except:
+                print(f"Error: Instruction {instruction_register} not found")
+                sys.exit(1)
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -9,3 +9,11 @@ cpu = CPU()
 
 cpu.load()
 cpu.run()
+
+if len(sys.argv) == 2:  # CPU file + file to be ran
+    cpu = CPU()
+    cpu.load(sys.argv[1])
+    cpu.run()
+else:
+    print('Error: Please provide filename to execute instructions')
+    sys.exit(1)
